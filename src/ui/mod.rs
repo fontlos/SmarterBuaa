31140cce8d029ccc0d30c03f06c85c1a51f887e7
Fontlos <fontlos@fontlos.com>
2025-05-13 21:59:50 +0800

    feat: Add login page

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,10 +1,19 @@
+mod login;
+
 use dioxus::prelude::*;
+
+#[derive(Routable, PartialEq, Clone)]
+enum Route{
+    #[route("/", login::LoginPage)]
+    Login {}
+}
 
 #[component]
 pub fn App() -> Element {
     #[cfg(feature = "desktop")]
     crate::config::tray_config();
     rsx! {
-        "Hello World"
+        document::Link { rel: "stylesheet", href: asset!("./assets/css/base.css") }
+        Router::<Route> {}
     }
 }
