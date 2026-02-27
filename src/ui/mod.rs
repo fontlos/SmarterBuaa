d842fc269b1790a182d1ddfc649977a42361e7c1
Fontlos <fontlos@fontlos.com>
2026-02-27 20:23:35 +0800

    fix: Font load failed

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -12,8 +12,10 @@ enum Route {
 pub fn App() -> Element {
     #[cfg(feature = "desktop")]
     crate::config::desktop::tray();
+
+    const FONT: Asset = asset!("/assets/font/Poppins-Regular.woff2", AssetOptions::builder().with_hash_suffix(false));
     rsx! {
-        document::Link { rel: "preload", href: "./font/Poppins-Regular.woff2", as: "font", type: "font/woff2"}
+        document::Link { rel: "preload", href: FONT, as: "font", type: "font/woff2"}
         document::Link { rel: "stylesheet", href: asset!("/assets/css/base.css") }
         Router::<Route> {}
     }
